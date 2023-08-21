f36bf4415f95aff47f743b59a114b5ca1058d1b7
Fatih Bayram <101430232+since1071@users.noreply.github.com>
2023-08-22 00:44:59 +0300

    Murat Yücedağ My Udemy Project Final

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,45 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            //Mail Gönderme İşlemleri
+
+            MimeMessage mimeMessage = new MimeMessage();
+            //Mail Kimden Gelecek
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HoteilerAdmin", "34fatih58@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            //Mail Kime Gidecek
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            //Mail İçerik
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("34fatih58@gmail.com", "swiliibiytpqakkk");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+            //Gönderilen Mailin Veri Tabanına Kaydedilmesi
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Controllers/ErrorPageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Error404
+            ()
+        {
+            return View();
+        }
+    }
+}
